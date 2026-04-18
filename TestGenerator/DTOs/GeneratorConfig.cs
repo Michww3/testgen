07d620cec21f710d02bfeb8874aa39604537437f
Артём Зимницкий <74270687+Michww3@users.noreply.github.com>
2026-04-18 04:26:08 +0300

    Initialize CLI

---
+++ TestGenerator/DTOs/GeneratorConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace TestGenerator.DTOs;
+
+public class GeneratorConfig
+{
+    public string? ClassName { get; set; }
+    public string? ModelClass { get; set; }
+    public string? ModelInterface { get; set; }
+    public List<TestParam>? Params { get; set; }
+}
---
+++ TestGenerator/DTOs/TestParam.cs
@@ -0,0 +1,19 @@
+﻿namespace TestGenerator.DTOs;
+
+public sealed record TestParam
+{
+    public string Type { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Init { get; set; } = string.Empty;
+    public string HashExpr { get; set; } = string.Empty;
+
+    public TestParam() { }
+
+    public TestParam(string type, string name, string init, string hashExpr)
+    {
+        Type = type ?? throw new ArgumentNullException(nameof(type));
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Init = init ?? throw new ArgumentNullException(nameof(init));
+        HashExpr = hashExpr ?? throw new ArgumentNullException(nameof(hashExpr));
+    }
+}
