d1cf4d4b2e92ddfd97eaf7c895fb44466e6aaab1
Michww3 <michww3@gmail.com>
2026-04-21 19:11:46 +0300

    Close #7, close #8

--- src/TestGenerator.cs
+++ src/TestGenerator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Testgen.Models;
 
@@ -6,134 +8,105 @@ namespace Testgen;
 
 public static class TestGenerator
 {
-#pragma warning disable IDE0058
     public static string Generate(GeneratorConfig config)
     {
         StringBuilder sb = new StringBuilder();
 
-        int n = config.Params.Length;
-        int total = 1 << n;
+        // 1. Test: from model
+        _ = sb.AppendLine(BuildModelTest(config));
+        _ = sb.AppendLine();
 
+        // 2. Test: all parameters combinations (2n)
+        int totalCombinations = 1 << config.Params.Length;
+        IOrderedEnumerable<int> combinations = Enumerable.Range(0, totalCombinations)
+            .OrderBy(CountBits)
+            .ThenBy(mask => mask);
+
+        foreach (int mask in combinations)
+        {
+            _ = sb.AppendLine(BuildParameterTest(config, mask));
+            _ = sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    private static string BuildModelTest(GeneratorConfig config)
+    {
         const string i1 = "    ";
         const string i2 = "        ";
 
-        //Model
-        sb.AppendLine("[Fact]");
-        sb.AppendLine("public void ProduceCorrectHashFromModel()");
-        sb.AppendLine("{");
+        string declarations = string.Join(Environment.NewLine,
+            config.Params.Select(p => $"{i1}{p.Type} {p.Name} = {p.Init};"));
 
-        foreach (Param param in config.Params)
-        {
-            sb.AppendLine($"{i1}{param.Type} {param.Name} = {param.Init};");
-        }
+        string modelArgs = string.Join($",{Environment.NewLine}",
+            config.Params.Select(p => $"{i2}{p.Name}"));
 
-        sb.AppendLine();
-
-        sb.AppendLine($"{i1}{config.ModelInterface} model = new {config.ModelHash}(");
-        for (int i = 0; i < n; i++)
-        {
-            string comma = i < n - 1 ? "," : "";
-            sb.AppendLine($"{i2}{config.Params[i].Name}{comma}");
-        }
-        sb.AppendLine($"{i1});");
-        sb.AppendLine();
-
-        sb.AppendLine($"{i1}{config.ModelName} expected = new {config.ModelName}(model);");
-        sb.AppendLine($"{i1}{config.ModelName} actual = new {config.ModelName}(model);");
-        sb.AppendLine();
-        sb.AppendLine($"{i1}Assert.True(expected.SequenceEqual(actual));");
-        sb.AppendLine("}");
-        sb.AppendLine();
-
-        //Ctor combinations
-        int[] masks = new int[total];
-        for (int i = 0; i < total; i++)
-        {
-            masks[i] = i;
-        }
-
-        Array.Sort(masks, (a, b) =>
-        {
-            int cmp = CountBits(a).CompareTo(CountBits(b));
-            return cmp != 0 ? cmp : a.CompareTo(b);
-        });
-
-        foreach (int mask in masks)
-        {
-            sb.AppendLine("[Fact]");
-            sb.AppendLine($"public void ProduceCorrectHashFrom{MaskName(mask, config.Params)}()");
-            sb.AppendLine("{");
-
-            foreach (Param param in config.Params)
-            {
-                sb.AppendLine($"{i1}{param.Type} {param.Name} = {param.Init};");
-            }
-
-            sb.AppendLine();
-
-            sb.AppendLine($"{i1}{config.ModelInterface} model = new {config.ModelHash}(");
-            for (int i = 0; i < n; i++)
-            {
-                string comma = i < n - 1 ? "," : "";
-                sb.AppendLine($"{i2}{config.Params[i].Name}{comma}");
-            }
-            sb.AppendLine($"{i1});");
-
-            sb.AppendLine();
-
-            sb.AppendLine($"{i1}{config.ModelName} expected = new {config.ModelName}(model);");
-
-            sb.AppendLine($"{i1}{config.ModelName} actual = new {config.ModelName}(");
-            for (int i = 0; i < n; i++)
-            {
-                bool isHash = (mask & (1 << i)) != 0;
-                string value = isHash ? config.Params[i].HashExpr : config.Params[i].Name;
-                string comma = i < n - 1 ? "," : "";
-                sb.AppendLine($"{i2}{value}{comma}");
-            }
-            sb.AppendLine($"{i1});");
-
-            sb.AppendLine();
-            sb.AppendLine($"{i1}Assert.True(expected.SequenceEqual(actual));");
-            sb.AppendLine("}");
-            sb.AppendLine();
-        }
-        return sb.ToString();
-    }
-#pragma warning restore IDE0058
-    private static int CountBits(int x)
-    {
-        int count = 0;
-        while (x != 0)
-        {
-            count += x & 1;
-            x >>= 1;
-        }
-        return count;
+        return $"[Fact]{Environment.NewLine}" +
+               $"public void ProduceCorrectHashFromModel(){Environment.NewLine}" +
+               $"{{{Environment.NewLine}" +
+               $"{declarations}{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}{config.ModelInterface} model = new {config.ModelHash}({Environment.NewLine}" +
+               $"{modelArgs}{Environment.NewLine}" +
+               $"{i1});{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}{config.ModelName} expected = new {config.ModelName}(model);{Environment.NewLine}" +
+               $"{i1}{config.ModelName} actual = new {config.ModelName}(model);{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}Assert.True(expected.SequenceEqual(actual));{Environment.NewLine}" +
+               $"}}";
     }
 
-    private static string MaskName(int mask, Param[] p)
+    private static string BuildParameterTest(GeneratorConfig config, int mask)
     {
-        if (mask == (1 << p.Length) - 1)
+        const string i1 = "    ";
+        const string i2 = "        ";
+
+        string declarations = string.Join(Environment.NewLine,
+            config.Params.Select(p => $"{i1}{p.Type} {p.Name} = {p.Init};"));
+
+        string modelArgs = string.Join($",{Environment.NewLine}",
+            config.Params.Select(p => $"{i2}{p.Name}"));
+
+        // HashExpr or Name depending on mask
+        string actualArgs = string.Join($",{Environment.NewLine}",
+            config.Params.Select((p, i) => $"{i2}{((mask & (1 << i)) != 0 ? p.HashExpr : p.Name)}"));
+
+        string suffix = GetTestNameSuffix(mask, config.Params);
+
+        return $"[Fact]{Environment.NewLine}" +
+               $"public void ProduceCorrectHashFrom{suffix}(){Environment.NewLine}" +
+               $"{{{Environment.NewLine}" +
+               $"{declarations}{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}{config.ModelInterface} model = new {config.ModelHash}({Environment.NewLine}" +
+               $"{modelArgs}{Environment.NewLine}" +
+               $"{i1});{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}{config.ModelName} expected = new {config.ModelName}(model);{Environment.NewLine}" +
+               $"{i1}{config.ModelName} actual = new {config.ModelName}({Environment.NewLine}" +
+               $"{actualArgs}{Environment.NewLine}" +
+               $"{i1});{Environment.NewLine}{Environment.NewLine}" +
+               $"{i1}Assert.True(expected.SequenceEqual(actual));{Environment.NewLine}" +
+               $"}}";
+    }
+
+    private static string GetTestNameSuffix(int mask, Param[] parameters)
+    {
+        if (mask == (1 << parameters.Length) - 1)
         {
             return "Hashes";
         }
 
-        string name = "";
+        IEnumerable<string> hashedNames = parameters
+            .Where((_, i) => (mask & (1 << i)) != 0)
+            .Select(p => char.ToUpper(p.Name[0], System.Globalization.CultureInfo.InvariantCulture) + p.Name[1..] + "Hash");
 
-        for (int i = 0; i < p.Length; i++)
-        {
-            if ((mask & (1 << i)) != 0)
-            {
-                name += UpperFirst(p[i].Name) + "Hash";
-            }
-        }
-
-        return name == "" ? "Values" : name;
+        // If mask == 0, return "Values"
+        return string.Concat(hashedNames.DefaultIfEmpty("Values"));
     }
 
-    private static string UpperFirst(string s)
+    private static int CountBits(int x)
     {
-        return char.ToUpper(s[0], System.Globalization.CultureInfo.InvariantCulture) + s[1..];
+        int count = 0;
+        while (x != 0)
+        { count += x & 1; x >>= 1; }
+        return count;
     }
 }
